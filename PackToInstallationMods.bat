d9f959a5452653ecb117cc0567c3a180ffae0ecd
Kumaezenjae <andrewjo93@gmail.com>
2022-03-23 03:52:32 -0400

    Added v0.1.2

--- PackToInstallationMods.bat
+++ PackToInstallationMods.bat
@@ -12,7 +12,7 @@ set /p packerdir=<PackerDir.txt
 rmdir /S /Q "%packerdir%\Input"
 mkdir "%packerdir%\Input"
 mkdir "%packerdir%\Input\Content"
-mkdir "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert"
+mkdir "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus"
 
 copy  /Y    "%thisdir%\WindowsNoEditor\FSD\AssetRegistry.bin" "%packerdir%\Input\AssetRegistry.bin"
 xcopy /Y /S "%thisdir%\WindowsNoEditor\FSD\Content\*" "%packerdir%\Input\Content\"
@@ -21,13 +21,13 @@ call %packerdir%\_Repack.bat "%packerdir%\Input"
 
 del "%packerdir%\autogen.txt"
 move /Y "%packerdir%\Input _P.pak" "%thisdir%\Releases\vLatestBuild\"
-move /Y "%thisdir%\Releases\vLatestBuild\Input _P.pak" "%thisdir%\Releases\vLatestBuild\ChatAlert.pak"
-move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak.4.bak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak.5.bak"
-move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak.3.bak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak.4.bak"
-move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak.2.bak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak.3.bak"
-move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak.1.bak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak.2.bak"
-move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak"       "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\ChatAlert.pak.1.bak"
-copy /Y "%thisdir%\Releases\vLatestBuild\ChatAlert.pak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\ChatAlert\"
+move /Y "%thisdir%\Releases\vLatestBuild\Input _P.pak" "%thisdir%\Releases\vLatestBuild\TeamStatus.pak"
+move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak.4.bak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak.5.bak"
+move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak.3.bak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak.4.bak"
+move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak.2.bak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak.3.bak"
+move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak.1.bak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak.2.bak"
+move /Y "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak"       "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\TeamStatus.pak.1.bak"
+copy /Y "%thisdir%\Releases\vLatestBuild\TeamStatus.pak" "c:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Mods\TeamStatus\"
 
 cd %orig_cwd%
 pause
